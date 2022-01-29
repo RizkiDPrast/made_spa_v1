79d0d3db6a87dc37cb45b4c40017413e9cf88069
RizkiDPrast <rizkidwip@gmail.com>
2022-01-29 11:40:18 +0700

    tdaction support mobile

--- src/components/TdAction.vue
+++ src/components/TdAction.vue
@@ -1,30 +1,66 @@
 <template>
   <q-td align="center" class="q-gutter-xs">
-    <slot> </slot>
-    <q-btn
-      text-color="primary"
-      :icon="editBtnIcon"
-      round
-      size="sm"
-      @click="edit"
-      :class="editBtnClass"
-    >
-      <q-tooltip content-class="bg-primary">
-        Edit
-      </q-tooltip>
-    </q-btn>
-    <q-btn
-      text-color="negative"
-      icon="las la-trash"
-      round
-      size="sm"
-      @click="del"
-      :class="deleteBtnClass"
-    >
-      <q-tooltip content-class="bg-negative">
-        Delete
-      </q-tooltip>
-    </q-btn>
+    <template v-if="$q.screen.gt.sm">
+      <slot> </slot>
+      <q-btn
+        text-color="primary"
+        :icon="editBtnIcon"
+        round
+        size="sm"
+        @click="edit"
+        :class="editBtnClass"
+      >
+        <q-tooltip content-class="bg-primary">
+          Edit
+        </q-tooltip>
+      </q-btn>
+      <q-btn
+        text-color="negative"
+        icon="las la-trash"
+        round
+        size="sm"
+        @click="del"
+        :class="deleteBtnClass"
+      >
+        <q-tooltip content-class="bg-negative">
+          Delete
+        </q-tooltip>
+      </q-btn>
+    </template>
+    <template v-else>
+      {{ $slots.sco }}
+      <q-btn icon="las la-ellipsis-v" round flat size="sm">
+        <q-menu>
+          <q-list dense>
+            <slot name="mobile"> </slot>
+            <q-item>
+              <q-btn
+                text-color="primary"
+                :icon="editBtnIcon"
+                flat
+                size="sm"
+                @click="edit"
+                :class="editBtnClass"
+                label="Edit"
+              ></q-btn>
+            </q-item>
+            <q-item>
+              <q-btn
+                text-color="negative"
+                icon="las la-trash"
+                flat
+                size="sm"
+                @click="del"
+                :class="deleteBtnClass"
+                label="Delete"
+              >
+              </q-btn>
+            </q-item>
+          </q-list>
+        </q-menu>
+      </q-btn>
+      <slot></slot>
+    </template>
   </q-td>
 </template>
 <script>
